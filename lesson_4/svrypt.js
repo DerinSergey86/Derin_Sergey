ac07ffce96c4be499d014cf0de4fc6f5ff8b47da
DerinSergey86 <svd86@bk.ru>
2025-10-23 00:23:34 +0300

    4/5 первого уровня

--- lesson_4/svrypt.js
+++ lesson_4/svrypt.js
@@ -3,8 +3,47 @@
 
 function sumTo(n) {
     let sum = 0;
-    for (let i = 0; i <= n; i++) {
+    for (let i = 1; i <= n; i++) {
 sum = sum + i;
     }
     return sum;
 }
+    let n = prompt ('укажите значение, до которого будет вычеслена сумма чисел начиная с 1', "10");
+console.log(sumTo(n));
+
+// 2. Степень числа
+// Напиши функцию pow(base, exponent) , которая возводит base в степень exponent
+// с помощью цикла.
+
+function pow(base, exponent) {
+    base = base ** exponent;
+    return base
+}
+let base = prompt ('укажите значение, которое будет возведено в степень', "2");
+let exponent = prompt ('укажите значение, в степень которого будет возведено число', "5");
+console.log(pow(base, exponent));
+
+// 3. Подсчёт чётных чисел
+// Функция countEven(n) должна вернуть количество чётных чисел от 1 до n.
+function countEven(n2) {
+    let evenNumbers = 0;
+    for (let i = 1; i <= n2; i++) {
+        if (i % 2 !== 0)continue;
+        evenNumbers ++
+    }
+    return evenNumbers;
+}
+let n2 = prompt ('укажите число, до которого будет вычеслено количество чётных чисел начиная с 1', "10");
+console.log(countEven(n2));
+
+// 4. Факториал числа
+// Реализуй factorial(n) с помощью цикла.
+function factorial(n3) {
+    let result = 1;
+    for (let i = 1; i <= n3; i++) {
+    result = result * i;
+    }
+    return result;
+}
+let n3 = prompt ('укажите число, до которого будет вычеслен факториал начиная с 1', "");
+console.log(factorial(n3));
